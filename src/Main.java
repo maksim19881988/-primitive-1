c1f4bcd29df1f4679769a373682e5482e6a60a60
maksim19881988 <89222634554m@gmail.com>
2024-11-30 11:43:02 +0500

    ini

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 1421;
+        int miles = 20;
+        int bonus = ticket / miles;
+        System.out.println("Количество начисленных бонусных миль : " + bonus);
+    }
+}
